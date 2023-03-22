5f517674786f5266163b17dcd2e6d1be669795e4
mkor92 <mkorhonen92@gmail.com>
2023-03-22 15:04:36 +0100

    vite and tailwind setup. Started boilerplate

---
+++ React-wordle-project/src/components/footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      <nav>
+        <ul>
+          <li>hej</li>
+          <li>hej</li>
+          <li>hej</li>
+          <li>hej</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
---
+++ React-wordle-project/src/components/header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div className="header w-full bg-slate-500 opacity-70 h-20 flex justify-center items-center">
+      <nav className="">
+        <ul className="flex justify-evenly">
+          <li>hej</li>
+          <li>hej</li>
+          <li>hej</li>
+          <li>hej</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
