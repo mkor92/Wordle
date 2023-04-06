33398f35f6ce2bde7fa9531fe01260b595ec656b
mkor92 <mkorhonen92@gmail.com>
2023-04-06 14:50:36 +0200

    GET from database to server api working

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,10 +1,41 @@
 import { Router } from "express";
 import words from "../getWords.js";
+import mongoose from "mongoose";
+import * as dotenv from "dotenv";
+import { list } from "../model.js";
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const example = new list({
+  name: "Markus",
+  time: 455,
+  guesses: 50,
+  wordLength: 1,
+  unique: false,
+});
+await example.save();
+const result = await list.find({});
+console.log(result);
+
+list.find().then((users) => {
+  console.log(users);
+});
 
 const router = Router();
 
 router.get("/words", async (req, res) => {
   res.json(words);
 });
+
+router.get("/highscore", async (req, res) => {
+  const highscoreList = await list.find({});
+  res.json(highscoreList);
+});
+
+router.post("highscore"),
+  async (req, res) => {
+    const listItem = await listItem.save();
+  };
 
 export default router;
