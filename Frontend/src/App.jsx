84e77a009d85432ec677369b9cfdabab70dbe604
mkor92 <mkorhonen92@gmail.com>
2023-04-17 13:35:21 +0200

    create board, chose length and get the word works

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -8,39 +8,6 @@ import Board from "./components/Board";
 function App() {
 
 
-  useEffect(() => {
-    async function getWords() {
-      let wordLength;
-      if (length == 5) {
-        wordLength = 5;
-
-      } else if (length == 6) {
-        wordLength = 6;
-
-      }
-      else if (length == 7) {
-        wordLength = 7;
-      }
-      else if (length == 8) {
-        wordLength = 8;
-      }
-      else if (length == 9) {
-        wordLength = 9;
-      }
-      else if (length == 10) {
-        wordLength = 10;
-      }
-      const payload = await GenerateWordSet(wordLength, unique)
-      setLength(wordLength)
-      setCorrectWord(payload.randomWord)
-
-      setWordSet(payload.wordSet);
-
-    }
-
-    getWords()
-
-  }, [(fireGetWords)])
 
 
 
--- Frontend/src/components/Board.jsx
+++ Frontend/src/components/Board.jsx
@@ -1,154 +1,166 @@
-
-import { useContext } from "react";
-import Letter from "./Letter";
-
-import { AppContext } from "../App";
+import { useState, useEffect } from "react"
+import GenerateWordSet from "./Words"
 
 
-export default function Board() {
+export default function Board(props) {
+  const [length, setLength] = useState(5)
+  const [unique, setUnique] = useState(false)
+  const [correctWord, setCorrectWord] = useState("")
+  const [wordSet, setWordSet] = useState(new Set())
+  const handleOnChange = (e) => {
+    setLength(e.target.value)
+  }
 
-  const { length, setLength } = useContext(AppContext)
+  const fireGetWords = length + unique
+  useEffect(() => {
+    async function getWords() {
+      let wordLength;
+      if (length == 5) {
+        wordLength = 5;
 
-  function createBoard5(val) {
-    return <div className="flex flex-row ">
-      <Letter letterPos={0} attemptVal={val} />
-      <Letter letterPos={1} attemptVal={val} />
-      <Letter letterPos={2} attemptVal={val} />
-      <Letter letterPos={3} attemptVal={val} />
-      <Letter letterPos={4} attemptVal={val} />
-    </div>
+      } else if (length == 6) {
+        wordLength = 6;
+
+      }
+      else if (length == 7) {
+        wordLength = 7;
+      }
+      else if (length == 8) {
+        wordLength = 8;
+      }
+      else if (length == 9) {
+        wordLength = 9;
+      }
+      else if (length == 10) {
+        wordLength = 10;
+      }
+      const payload = await GenerateWordSet(wordLength, unique)
+      setLength(wordLength)
+      setCorrectWord(payload.randomWord)
+
+      setWordSet(payload.wordSet);
+
+    }
+
+    getWords()
+
+  }, [fireGetWords])
+
+  const handleUnique = () => {
+    if (unique === false) {
+      setUnique(true)
+      setLength(length)
+    } else {
+      setUnique(false)
+      setLength(length)
+    }
   }
-  function createBoard6(val) {
-    return <div className="flex flex-row ">
-      <Letter letterPos={0} attemptVal={val} />
-      <Letter letterPos={1} attemptVal={val} />
-      <Letter letterPos={2} attemptVal={val} />
-      <Letter letterPos={3} attemptVal={val} />
-      <Letter letterPos={4} attemptVal={val} />
-      <Letter letterPos={5} attemptVal={val} />
-    </div>
-  }
-  function createBoard7(val) {
-    return <div className="flex flex-row ">
-      <Letter letterPos={0} attemptVal={val} />
-      <Letter letterPos={1} attemptVal={val} />
-      <Letter letterPos={2} attemptVal={val} />
-      <Letter letterPos={3} attemptVal={val} />
-      <Letter letterPos={4} attemptVal={val} />
-      <Letter letterPos={5} attemptVal={val} />
-      <Letter letterPos={6} attemptVal={val} />
-    </div>
-  }
-  function createBoard8(val) {
-    return <div className="flex flex-row ">
-      <Letter letterPos={0} attemptVal={val} />
-      <Letter letterPos={1} attemptVal={val} />
-      <Letter letterPos={2} attemptVal={val} />
-      <Letter letterPos={3} attemptVal={val} />
-      <Letter letterPos={4} attemptVal={val} />
-      <Letter letterPos={5} attemptVal={val} />
-      <Letter letterPos={6} attemptVal={val} />
-      <Letter letterPos={7} attemptVal={val} />
-    </div>
-  }
-  function createBoard9(val) {
-    return <div className="flex flex-row ">
-      <Letter letterPos={0} attemptVal={val} />
-      <Letter letterPos={1} attemptVal={val} />
-      <Letter letterPos={2} attemptVal={val} />
-      <Letter letterPos={3} attemptVal={val} />
-      <Letter letterPos={4} attemptVal={val} />
-      <Letter letterPos={5} attemptVal={val} />
-      <Letter letterPos={6} attemptVal={val} />
-      <Letter letterPos={7} attemptVal={val} />
-      <Letter letterPos={8} attemptVal={val} />
-    </div>
-  }
-  function createBoard10(val) {
-    return <div className="flex flex-row ">
-      <Letter letterPos={0} attemptVal={val} />
-      <Letter letterPos={1} attemptVal={val} />
-      <Letter letterPos={2} attemptVal={val} />
-      <Letter letterPos={3} attemptVal={val} />
-      <Letter letterPos={4} attemptVal={val} />
-      <Letter letterPos={5} attemptVal={val} />
-      <Letter letterPos={6} attemptVal={val} />
-      <Letter letterPos={7} attemptVal={val} />
-      <Letter letterPos={8} attemptVal={val} />
-      <Letter letterPos={9} attemptVal={val} />
-    </div>
-  }
-  if (length == 5) {
+
+  function createBoard(num) {
     return (
-      <div className="board">
-        {createBoard5(0)}
-        {createBoard5(1)}
-        {createBoard5(2)}
-        {createBoard5(3)}
-        {createBoard5(4)}
-        {createBoard5(5)}
-
-      </div>
-    );
-  } else if (length == 6) {
-    return (
-      <div className="board">
-        {createBoard6(0)}
-        {createBoard6(1)}
-        {createBoard6(2)}
-        {createBoard6(3)}
-        {createBoard6(4)}
-        {createBoard6(5)}
-
-      </div>
-    );
-  } else if (length == 7) {
-    return (
-      <div className="board">
-        {createBoard7(0)}
-        {createBoard7(1)}
-        {createBoard7(2)}
-        {createBoard7(3)}
-        {createBoard7(4)}
-        {createBoard7(5)}
-
-      </div>
-    );
-  } else if (length == 8) {
-    return (
-      <div className="board">
-        {createBoard8(0)}
-        {createBoard8(1)}
-        {createBoard8(2)}
-        {createBoard8(3)}
-        {createBoard8(4)}
-        {createBoard8(5)}
-
-      </div>
-    );
-  } else if (length == 9) {
-    return (
-      <div className="board">
-        {createBoard9(0)}
-        {createBoard9(1)}
-        {createBoard9(2)}
-        {createBoard9(3)}
-        {createBoard9(4)}
-        {createBoard9(5)}
-
-      </div>
-    );
-  } else if (length == 10) {
-    return (
-      <div className="board">
-        {createBoard10(0)}
-        {createBoard10(1)}
-        {createBoard10(2)}
-        {createBoard10(3)}
-        {createBoard10(4)}
-        {createBoard10(5)}
-
-      </div>
-    );
+      Array.from({ length: num }, (_, i) => <div key={i} className="h-7 w-7 sm:h-14 sm:w-14 bg-slate-600 m-1 flex flex-col justify-center items-center text-1xl font-bold sm:text-4xl rounded">
+      </div>)
+    )
   }
+
+  const createBoardLength = (num) => {
+    return (
+      <div className="flex flex-row">
+        {createBoard(num)}
+      </div>
+    )
+  }
+
+  const getBoardLength = () => {
+    if (length == 5) {
+      return (
+        <div className="board flex flex-col items-center">
+          {createBoardLength(5)}
+          {createBoardLength(5)}
+          {createBoardLength(5)}
+          {createBoardLength(5)}
+          {createBoardLength(5)}
+          {createBoardLength(5)}
+        </div>
+      );
+    } else if (length == 6) {
+      return (
+        <div className="board flex flex-col items-center">
+          {createBoardLength(6)}
+          {createBoardLength(6)}
+          {createBoardLength(6)}
+          {createBoardLength(6)}
+          {createBoardLength(6)}
+          {createBoardLength(6)}
+        </div>
+      );
+    } else if (length == 7) {
+      return (
+        <div className="board flex flex-col items-center">
+          {createBoardLength(7)}
+          {createBoardLength(7)}
+          {createBoardLength(7)}
+          {createBoardLength(7)}
+          {createBoardLength(7)}
+          {createBoardLength(7)}
+        </div>
+      );
+    } else if (length == 8) {
+      return (
+        <div className="board flex flex-col items-center">
+          {createBoardLength(8)}
+          {createBoardLength(8)}
+          {createBoardLength(8)}
+          {createBoardLength(8)}
+          {createBoardLength(8)}
+          {createBoardLength(8)}
+        </div>
+      );
+    } else if (length == 9) {
+      return (
+        <div className="board flex flex-col items-center">
+          {createBoardLength(9)}
+          {createBoardLength(9)}
+          {createBoardLength(9)}
+          {createBoardLength(9)}
+          {createBoardLength(9)}
+          {createBoardLength(9)}
+        </div>
+      );
+    } else if (length == 10) {
+      return (
+        <div className="board flex flex-col items-center">
+          {createBoardLength(10)}
+          {createBoardLength(10)}
+          {createBoardLength(10)}
+          {createBoardLength(10)}
+          {createBoardLength(10)}
+          {createBoardLength(10)}
+        </div>
+      );
+    }
+  }
+  return (
+
+    < div id={length} className="" >
+
+      <label htmlFor="uniqueLetters" className="mr-4 text-lg">Only use words with unique letters</label>
+      <input type="checkbox" onChange={handleUnique} id="uniqueLetters" className="h-5 w-5  rounded shadow bg-green-600" />
+
+      <div className="flex mt-2 mb-4">
+        <p className="mr-4 text-lg">How many letters?</p>
+        <select className="w-10 bg-gray-400 rounded text-center text-base shadow" value={length} onChange={handleOnChange}>
+          <option value="5">5</option>
+          <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+        </select>
+      </div>
+
+      {getBoardLength()}
+    </div >
+  )
+
 }
